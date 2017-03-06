80414850c2b46e36a10c20860853c76a6f2db0a2
Marianna Veríssimo <marianna1204@gmail.com>
2017-03-06 10:45:22 -0300

    Add arquivos de inicialização do banco

--- hsqldb/starttweb.bat
+++ hsqldb/starttweb.bat
@@ -1,8 +1,8 @@
 ﻿@echo off
 rem Modifique a variável abaixo para o local onde está a pasta do seu banco de dados.
-set BD_HOME=C:\Users\marianna\Desktop\pratica03\bd
+set BD_HOME=/home/marianna/Área de Trabalho/Pratica03JSF/bd
 echo =========
 echo Local do banco de dados: %BD_HOME%
 echo =========
 
-java -cp .\lib\hsqldb.jar org.hsqldb.server.Server --database.0 file:%BD_HOME%\turmasweb\turmasweb --dbname.0 tweb
+java -cp ./lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:%BD_HOME%/turmasweb/turmasweb --dbname.0 tweb
---
+++ hsqldb/turmas.sh
@@ -0,0 +1,1 @@
+java -cp ./lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:./turmasweb/turmasweb --dbname.0 tweb
